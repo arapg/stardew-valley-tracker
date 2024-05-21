796f20a4606c5b7a31ca4855e90dc80d91784a83
arapg <hello@arpg.dev>
2024-05-21 18:40:12 +0200

    added API route to get completed items for a user

---
+++ src/app/api/user/items/[userID]/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server"
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export async function GET(
+    req: Request,
+    { params }: { params: { userID: string } }
+) {
+    try {
+        const items = await prisma.completedItem.findMany({
+            where: { farmerId: params.userID }
+        })
+        console.log(items)
+        return NextResponse.json(items)
+    } catch (error) {
+        console.log(error)
+        return new NextResponse(
+            `Something went wrong when getting the items.`,
+            { status: 500 }
+        )
+    }
+}
