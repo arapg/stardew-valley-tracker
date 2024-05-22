640b23a2e11034558771ee673d53871620b88c6b
arapg <hello@arpg.dev>
2024-05-22 21:47:34 +0200

    added Achievements

---
+++ src/app/Achievements.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import useUserIDStore from './states/userID'
+import useCompletedAchievementsStore from './states/completedAchievements'
+import AchievementCard from './components/AchievementCard'
+
+export interface Achievement {
+	id: number
+	name: string
+	description: string
+	url: string
+}
+
+export default function Achievements() {
+	const { userID, setUserID } = useUserIDStore()
+	const [achievements, setAchievements] = useState<Achievement[]>([])
+	const [completedAchievements, setCompletedAchievements] = useState<number[]>(
+		[],
+	)
+	const { refetchCompletedAchievements, setRefetchCompletedAchievements } =
+		useCompletedAchievementsStore()
+
+	async function fetchUser() {
+		try {
+			const response = await fetch('/api/auth/me', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			setUserID(data.sub)
+		} catch (error) {
+			console.log(error)
+		}
+		setRefetchCompletedAchievements(true)
+	}
+
+	async function fetchAchievements() {
+		try {
+			const response = await fetch('/api/achievements', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			setAchievements(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	async function fetchCompletedAchievements() {
+		try {
+			const response = await fetch(`/api/user/achievements/${userID}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			const ids = data.map((achievement: { id: number }) => achievement.id) // Extracting IDs
+			setCompletedAchievements(ids)
+			setRefetchCompletedAchievements(false)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchUser()
+		fetchAchievements()
+	}, [])
+
+	useEffect(() => {
+		if (userID) {
+			fetchCompletedAchievements()
+		}
+	}, [userID, refetchCompletedAchievements])
+
+	return (
+		<div>
+			<h1>Achievements</h1>
+			<p>Coming soon...</p>
+			{achievements.map((achievement) => (
+				<AchievementCard
+					key={achievement.id}
+					achievement={achievement}
+					completedAchievements={completedAchievements}
+				/>
+			))}
+		</div>
+	)
+}
---
+++ src/app/components/AchievementCard.tsx
@@ -0,0 +1,55 @@
+import { Achievement } from '../Achievements'
+import useUserIDStore from '../states/userID'
+import useCompletedAchievementsStore from '../states/completedAchievements'
+import { useEffect, useState } from 'react'
+
+interface AchievementCardProps {
+	achievement: Achievement
+	completedAchievements: number[]
+}
+
+export default function AchievementCard({
+	achievement,
+	completedAchievements,
+}: AchievementCardProps) {
+	const { userID } = useUserIDStore()
+	const { refetchCompletedAchievements, setRefetchCompletedAchievements } =
+		useCompletedAchievementsStore()
+	const [achievementIsCompleted, setAchievementIsCompleted] = useState(false)
+
+	useEffect(() => {
+		setAchievementIsCompleted(
+			completedAchievements.includes(achievement.id) ? true : false,
+		)
+	}, [completedAchievements, achievement])
+
+	function handleClick(achievementID: number) {
+		try {
+			fetch(`/api/save/achievement`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ userID, achievementID }),
+			}).then(() => {
+				setRefetchCompletedAchievements(!refetchCompletedAchievements)
+			})
+		} catch (error) {
+			console.error(error)
+		}
+	}
+	return (
+		<div
+			className='achievement-card'
+			onClick={() => handleClick(achievement.id)}
+		>
+			<div className={`card-info ${achievementIsCompleted ? 'completed' : ''}`}>
+				<img src={achievement.url} alt={achievement.name} />
+				<div className='card-title'>
+					<h2>{achievement.name}</h2>
+					<p>{achievement.description}</p>
+				</div>
+			</div>
+		</div>
+	)
+}
