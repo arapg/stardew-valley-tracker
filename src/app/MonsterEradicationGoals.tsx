35d48258410c2d911abcfb9431c97086f81d72a6
arapg <hello@arpg.dev>
2024-05-22 21:47:45 +0200

    added Monster Eradication Goals

---
+++ src/app/MonsterEradicationGoals.tsx
@@ -0,0 +1,104 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import useUserIDStore from './states/userID'
+import useCompletedEradicationGoalsStore from './states/completedEradicationGoals'
+import EradicationGoalCard from './components/EradicationCard'
+
+export interface MonsterEradicationGoal {
+	id: number
+	monster: string
+	quantity: number
+	reward: string
+	url: string
+}
+
+export default function MonsterEradicationGoals() {
+	const { userID, setUserID } = useUserIDStore()
+	const [eradicationGoals, setEradicationGoals] = useState<
+		MonsterEradicationGoal[]
+	>([])
+	const [completedEradicationGoals, setCompletedEradicationGoals] = useState<
+		number[]
+	>([])
+	const {
+		refetchCompletedEradicationGoals,
+		setRefetchCompletedEradicationGoals,
+	} = useCompletedEradicationGoalsStore()
+
+	async function fetchUser() {
+		try {
+			const response = await fetch('/api/auth/me', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			setUserID(data.sub)
+		} catch (error) {
+			console.log(error)
+		}
+		setRefetchCompletedEradicationGoals(true)
+	}
+
+	async function fetchEradicationGoals() {
+		try {
+			const response = await fetch('/api/eradication-goals', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			setEradicationGoals(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	async function fetchCompletedEradicationGoals() {
+		try {
+			const response = await fetch(`/api/user/eradications/${userID}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			// Extracting IDs
+			const ids = data.map(
+				(eradicationGoal: { id: number }) => eradicationGoal.id,
+			)
+			setCompletedEradicationGoals(ids)
+			setRefetchCompletedEradicationGoals(false)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchUser()
+		fetchEradicationGoals()
+	}, [])
+
+	useEffect(() => {
+		if (userID) {
+			fetchCompletedEradicationGoals()
+		}
+	}, [userID, refetchCompletedEradicationGoals])
+
+	return (
+		<div>
+			<h1>EradicationGoals</h1>
+			<p>Coming soon...</p>
+			{eradicationGoals.map((eradicationGoal) => (
+				<EradicationGoalCard
+					key={eradicationGoal.id}
+					eradicationGoal={eradicationGoal}
+					completedEradicationGoals={completedEradicationGoals}
+				/>
+			))}
+		</div>
+	)
+}
---
+++ src/app/components/EradicationCard.tsx
@@ -0,0 +1,71 @@
+import { MonsterEradicationGoal } from '../MonsterEradicationGoals'
+import useUserIDStore from '../states/userID'
+import useCompletedEradicationGoalsStore from '../states/completedEradicationGoals'
+import { useEffect, useState } from 'react'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+
+interface EradicationCardProps {
+	eradicationGoal: MonsterEradicationGoal
+	completedEradicationGoals: number[]
+}
+
+export default function EradicationCard({
+	eradicationGoal,
+	completedEradicationGoals,
+}: EradicationCardProps) {
+	const { userID } = useUserIDStore()
+	const {
+		refetchCompletedEradicationGoals,
+		setRefetchCompletedEradicationGoals,
+	} = useCompletedEradicationGoalsStore()
+	const [eradicationGoalIsCompleted, setEradicationGoalIsCompleted] =
+		useState(false)
+
+	useEffect(() => {
+		setEradicationGoalIsCompleted(
+			completedEradicationGoals.includes(eradicationGoal.id) ? true : false,
+		)
+	}, [completedEradicationGoals, eradicationGoal])
+
+	function handleClick(monsterID: number) {
+		try {
+			fetch(`/api/save/monster`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ userID, monsterID }),
+			}).then(() => {
+				setRefetchCompletedEradicationGoals(!refetchCompletedEradicationGoals)
+			})
+		} catch (error) {
+			console.error(error)
+		}
+	}
+	return (
+		<ResponsiveMasonry>
+			<Masonry>
+				<div
+					className='achievement-card'
+					onClick={() => handleClick(eradicationGoal.id)}
+				>
+					<div
+						className={`card-info ${
+							eradicationGoalIsCompleted ? 'completed' : ''
+						}`}
+					>
+						<img src={eradicationGoal.url} alt={eradicationGoal.monster} />
+						<div className='card-title'>
+							<h2>{eradicationGoal.monster}</h2>
+							<p>{`Kill ${eradicationGoal.quantity} ${eradicationGoal.monster}`}</p>
+							<p>
+								<strong>Reward: </strong>
+								{eradicationGoal.reward}
+							</p>
+						</div>
+					</div>
+				</div>
+			</Masonry>
+		</ResponsiveMasonry>
+	)
+}
