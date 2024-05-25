72ede19129de505dadd491f044558b5b13ea9f8b
arapg <hello@arpg.dev>
2024-05-25 11:47:13 +0200

    moved Logout button to UserCard

--- src/app/components/LogoutButton.tsx
+++
@@ -1,9 +0,0 @@
-export default function LogoutButton() {
-	return (
-		<div className='logout-container'>
-			<a href='/api/auth/logout'>
-				<button className='logout'>Log Out</button>
-			</a>
-		</div>
-	)
-}
