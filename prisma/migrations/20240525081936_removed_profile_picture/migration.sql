d744b2891a73728137f9be148b5393d0ce3734a3
arapg <hello@arpg.dev>
2024-05-25 11:36:29 +0200

    added default placeholder profile picture

---
+++ prisma/migrations/20240525081936_removed_profile_picture/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `profilePicture` on the `Farmer` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Farmer" DROP COLUMN "profilePicture";
