a700a925c4ebf3a649cddf91e974323a5feadce7
arapg <hello@arpg.dev>
2024-05-25 11:36:55 +0200

    removed logout button from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 				{!user && <Landing />}
 				{user && (
 					<>
-						<Sidebar /> <LogoutButton />
+						<Sidebar />
 					</>
 				)}
 				{user && page === 'bundles' && <Bundles />}
